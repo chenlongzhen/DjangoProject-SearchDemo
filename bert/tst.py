5efa96b5de3c2a9a704e5717f3939b467c5efcd2
陈龙震 <760950023@qq.com>
2020-12-08 17:16:39 +0800

    bert召回 2

---
+++ bert/tst.py
@@ -0,0 +1,8 @@
+#!/bin/env python
+
+from bert_serving.client import BertClient
+bc = BertClient()
+a=bc.encode(['First do it', 'then do it right', 'then do it better'])
+
+b = bc.encode(['First do it ||| then do it right'])
+print(b)
