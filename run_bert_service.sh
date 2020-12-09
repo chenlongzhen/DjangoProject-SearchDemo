830e68e47a11d1026693f3146f91fe3ad646fa72
chenlongzhen <760950023@qq.com>
2020-12-09 02:55:12 +0000

    docker fix

--- run_bert_service.sh
+++ run_bert_service.sh
@@ -1,8 +1,11 @@
 #!/bin/env bash
+source ~/.bashrc
 
-ps -a | grep  "bert-serving-start"
+ps -a | grep  "bert-serv"
 echo "如果已经有相同服务启动请手动kill再重新启动"
 sleep 3s
+
+conda activate py3
 # bert server
 echo "启动bertservice..."
 nohup bert-serving-start -model_dir ./bert/chinese_L-12_H-768_A-12/ -num_worker=1 -port=4000 -port_out=4001 > bert_server.log &
--- run_web.sh
+++ run_web.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
+source ~/.bashrc
 
 echo "启动web..."
+conda activate py3
 python manage.py makemigrations 
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
